18a3ba566d7c296aecf61494474938224a58ee9c
ManiDevops <manireddydevops@gmail.com>
2022-12-24 22:40:14 +0530

    Adding the files to repo

--- app/src/main/java/com/mani/calccdemoapp/MainActivity.kt
+++ app/src/main/java/com/mani/calccdemoapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.mani.calccdemoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.tvSubmit).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+
+        Toast.makeText(this,"Hi",Toast.LENGTH_SHORT).show()
     }
 }
